3757fe0f526c6dab1e51f7fd6e0fedd21ecda5a8
Jorge Luis <jorgequiguango@outlook.com>
2023-10-03 10:45:14 -0500

    [REF] Name of file

--- pyside/pos/ui_mainwindow.py.py
+++ pyside/pos/ui_mainwindow.py.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'form.ui'
+## Form generated from reading UI file 'mainwindow.py.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.5.2
 ##
