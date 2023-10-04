eb1a7c04531703a03625b295379fd000ff1415b7
Jorge Luis <jorgequiguango@outlook.com>
2023-10-04 14:58:08 -0500

    [IMP] Get product

--- pyside/pos/views/point_of_sale.py
+++ pyside/pos/views/point_of_sale.py
@@ -1,3 +1,6 @@
+import os
+from xmlrpc import client
+
 from PySide6.QtWidgets import QMainWindow
 from PySide6.QtGui import QCloseEvent
 
@@ -10,12 +13,37 @@ class PontOfSale(QMainWindow):
         self.ui = Ui_PosWindow()
         self.ui.setupUi(self)
         self.ui.pushAdd.clicked.connect(self.add_row)
+        self.ui.tableDetail.insertRow(0)
+        self.ui.tableDetail.itemChanged.connect(self.changed)
 
     def closeEvent(self, event: QCloseEvent) -> None:
         super().close()
         self.parent().show()
 
     def add_row(self):
-        rowPosition = self.ui.tableDetail.rowCount()
-        self.ui.tableDetail.insertRow(rowPosition)
+        row_position = self.ui.tableDetail.rowCount()
+        self.ui.tableDetail.insertRow(row_position)
         print('Add row')
+        server = os.getenv('SERVER')
+        db = os.getenv('DB')
+        uid = os.getenv('UID')
+        password = os.getenv('PASSWORD')
+        self.get_product(server, db, uid, password)
+
+    def changed(self):
+        print('item Changed')
+
+    def get_product(self, server, db, uid, password):
+        api = client.ServerProxy('%s/xmlrpc/2/object' % server)
+        companies = (api
+                     .execute_kw(db, uid, password,
+                                   'product.product',
+                                   'search_read',
+                                   [[['barcode', '=', '5651234567866']]],
+                                   {}))
+
+        for c in companies:
+            print(c)
+            print(str(c['id']), c['name'])
+
+
